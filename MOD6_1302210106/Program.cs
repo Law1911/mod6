15d5605fad29f64af81734e90389043467f00cea
Praktikan <126835746+Law1911@users.noreply.github.com>
2023-03-27 07:14:21 +0700

    first commit

---
+++ MOD6_1302210106/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MOD6_1302210106
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!sdfsdf");
+        }
+    }
+}
---
+++ MOD6_1302210106/SayaTubeUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOD6_1302210106
+{
+    internal class SayaTubeUser
+    {
+    }
+}
